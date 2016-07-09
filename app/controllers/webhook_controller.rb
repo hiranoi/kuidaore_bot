5eb7b1a7aa82eaef66674b16b92eb6c817026478
平野一生 <hiranoi@pro.local>
2016-07-09 21:44:56 +0900

    テスト

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -21,7 +21,7 @@ class WebhookController < ApplicationController
     res_message = repl_client.get_message('uzQWHcjUNYoJCSaongHabkPHA3xh7lba', req_message)
 
     client = LineClient.new(CHANNEL_ID, CHANNEL_SECRET, CHANNEL_MID, OUTBOUND_PROXY)
-    res = client.send([from_mid], ConvertToOsaka.new(res_message).convert)
+    res = client.send([from_mid], ConvertToOsaka.new(res_message['systemText']['expression']).convert)
 
     if res.status == 200
       logger.info({success: res})
